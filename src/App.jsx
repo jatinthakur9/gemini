2e1b34af45084e24d03a7c28006807fa5816ce65
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-01-23 08:23:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SideBar from "./components/SideBar/SideBar";
+import Main from "./components/Main/Main";
+
+function App() {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
