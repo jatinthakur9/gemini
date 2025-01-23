899f6865f66cc14dd3ad8d5e97e9b332e51e4b0f
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-01-23 08:50:19 +0530

    Completed the UI PART

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -35,6 +35,22 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter  a prompt here" />
+
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display innaccurate info , including about people , so
+            double-check its response . Your privacy and Gemini Apps
+          </p>
+        </div>
       </div>
     </div>
   );
